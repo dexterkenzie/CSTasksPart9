1f12a7babca19116c260b8204889a37969120c21
«dashkovsky» <>
2022-10-02 18:51:44 +0300

    task 64 done

--- task64/Program.cs
+++ task64/Program.cs
@@ -4,3 +4,17 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
+
+// Метод вывода натуральных чисел от N до 1:
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не соответствует условию.");
+    if (number == 0) return;
+    Console.Write($"{number}  ");
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
